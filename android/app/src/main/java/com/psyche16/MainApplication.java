0d3250ff976c44b586759dd9a7a855291e712cef
namebar <fernangonzalescool@gmail.com>
2020-02-26 20:15:41 -0700

    Android Set up for React-Native Unity View, and Implementation of Unity for React-Native

--- android/app/src/main/java/com/psyche16/MainApplication.java
+++ android/app/src/main/java/com/psyche16/MainApplication.java
@@ -3,6 +3,7 @@ package com.psyche16;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.unity.view.UnityViewPackage;
 import com.horcrux.svg.SvgPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new UnityViewPackage(),
             new SvgPackage()
       );
     }
--- app/screens/GamePlayer.js
+++ app/screens/GamePlayer.js
@@ -24,16 +24,19 @@ import infoPageStyle from '../styles/InfoPage.style';
 import GameStyle from '../styles/Game.style';
 import {Fonts} from '../components/Fonts';
 
+import UnityView from 'react-native-unity-view'
+
 class GamePlayer extends Component {
 	static navigationOptions = {
-		headerStyle: headerStyle.sectionHeadingStyle
+		headerMode: 'none',
+		header: null,
 	};
 	
 	render() {
 		return (
-			<Container>
-			
-			</Container>
+			<View style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, }} >
+				<UnityView style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, }} />
+			</View>
 		);
 	}
 }
